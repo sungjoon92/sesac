4bf9b1ccc10e1911b909147cb86e918fd56ba69b
sungjoon92 <wsjtjdwns92@naver.com>
2024-10-24 18:49:55 +0900

    while 배열의 요소를 활용하여 출석번호 구하기

---
+++ javascript/1022/실습/출석.js
@@ -0,0 +1,9 @@
+const names = ['jun', 'beemo', 'ken', 'lynda'];
+
+let index = 0;
+while (index < names.length) {
+  let name = names[index];
+  // let 출석번호 = index+1
+  console.log(index + 1, name);
+  index++;
+}
