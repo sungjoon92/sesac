a6ff2c63451ac8e2d01afb1ce6f46580321cb378
sungjoon92 <wsjtjdwns92@naver.com>
2024-10-24 17:53:34 +0900

    얕은복사, 깊은복사

---
+++ javascript/1024/객체_copy.js
@@ -0,0 +1,35 @@
+let arr_1 = [1, 2, 3];
+let arr_2 = arr_1;
+let arr_3 = [...arr_1];
+
+arr_1[1] = 100;
+
+console.log(arr_1); // [ 1, 100, 3 ]
+console.log(arr_2); // [ 1, 100, 3 ]
+console.log(arr_3); //[ 1, 2, 3 ]
+
+let mat_1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+let mat_2 = mat_1;
+let mat_3 = [...mat_1];
+
+mat_1[1][1] = 100;
+
+console.log(mat_1); // 바뀜
+console.log(mat_2); // 바뀜
+console.log(mat_3); // 바뀜
+
+mat_1[1] = 100;
+
+console.log(mat_1); // 바뀜
+console.log(mat_2); // 바뀜
+console.log(mat_3); // 안 바뀜
+
+// let mat_4 = [...mat_1];
+let mat_4 = [];
+for (let el of mat_1) {
+  mat_4.push(el);
+}
