29daa49518c626015032598b629ee276376a693b
sungjoon92 <wsjtjdwns92@naver.com>
2024-10-24 17:55:42 +0900

    논리 연산자 &&, || 파보기

---
+++ javascript/1024/logic.js
@@ -0,0 +1,8 @@
+// 논리 연산자에서, 해당 값이 정확히 무엇인지 판단할 수 있을 때의 값이 반환된다.
+console.log(1 || 2);
+console.log(3 || 2);
+console.log(0 || 2);
+
+console.log(1 && 2);
+console.log(0 && 2);
+console.log(2 && 0);
