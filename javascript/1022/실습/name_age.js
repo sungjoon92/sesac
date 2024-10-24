79c032c51816ac88e220b6b76ddf86e140df8617
sungjoon92 <wsjtjdwns92@naver.com>
2024-10-24 18:51:13 +0900

    증감식 반복문을 활용한 배열의 이름, 나이 구하기

---
+++ javascript/1022/실습/name_age.js
@@ -0,0 +1,9 @@
+const names = ['jun', 'beemo', 'ken'];
+const ages = [18, 28, 37];
+
+for (let i = 0; i < names.length; i++) {
+  const name = names[i];
+  const age = ages[i];
+
+  console.log(name, age);
+}
