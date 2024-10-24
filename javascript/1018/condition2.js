8e59dc1706da5a91166e75d7a74a81a2dcc3ecf8
sungjoon92 <wsjtjdwns92@naver.com>
2024-10-24 18:05:11 +0900

    if문 실습2

---
+++ javascript/1018/condition2.js
@@ -0,0 +1,50 @@
+// boolean
+if (true) {
+  console.log(true);
+}
+
+if (false) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// 비교연산자
+
+if (1 < 2) {
+  console.log('1 < 2');
+}
+
+// 논리 연산자
+if (true && true) {
+  console.log('true && true');
+}
+
+// 숫자도 가능
+if (1) {
+  console.log(1);
+}
+
+if (-1) {
+  console.log(-1);
+}
+
+if (0) {
+  console.log('실행 안됨');
+}
+
+if ('abc') {
+  console.log('abc');
+}
+
+if ('') {
+  console.log('빈 문자열');
+}
+
+if (' ') {
+  console.log('공백');
+}
+
+if (null) {
+  console.log('null');
+}
