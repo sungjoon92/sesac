dbac62a1bac1373c274dd155c9444778f845139b
sungjoon92 <wsjtjdwns92@naver.com>
2024-10-28 19:38:42 +0900

    api 새로운 영화정보 배열에 삽입

---
+++ javascript/1028/tmdb_a_additional.js
@@ -0,0 +1,63 @@
+async function getNowPlayingMovies() {
+  try {
+    const baseURL = 'https://api.themoviedb.org/3/movie';
+    const path = '/now_playing';
+    const params = new URLSearchParams({
+      api_key: 'eab8c9893e725b2e167187cef66bae3d',
+      language: 'ko',
+    });
+
+    const URL = `${baseURL}${path}?${params}`;
+
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    const movies = data.results;
+    const my_movie = {
+      title: 'sesac the movie',
+
+      vote_average: 10.0,
+      vote_count: 1,
+    };
+
+    movies.push(my_movie);
+
+    let maxVoteAverage = 0;
+    let maxVoteMovie = null;
+    let minVoteCount = 100;
+
+    for (let movie of movies) {
+      let voteAverage = movie.vote_average;
+      let voteCount = movie.vote_count;
+
+      if (voteCount < minVoteCount) continue;
+
+      if (voteAverage > maxVoteAverage) {
+        maxVoteAverage = voteAverage;
+        maxVoteMovie = movie;
+      }
+      // if (voteAverage > maxVoteAverage) {
+      //   if (voteCount < minVoteCount) continue;
+      //   maxVoteAverage = voteAverage;
+      //   maxVoteMovie = movie;
+      // }
+      // if (voteAverage > maxVoteAverage && voteCount > minVoteCount) {
+      //   maxVoteAverage = voteAverage;
+      //   maxVoteMovie = movie;
+      // }
+      // if (voteAverage > maxVoteAverage) {
+      //   if (voteCount > minVoteCount){
+      //     maxVoteAverage = voteAverage;
+      //     maxVoteMovie = movie;
+      //   }
+      // }
+    }
+
+    console.log(maxVoteAverage);
+    console.log(maxVoteMovie.title);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+getNowPlayingMovies();
