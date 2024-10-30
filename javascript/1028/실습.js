03416f5ab1fccd9bf700ae53102dea1447ce4832
sungjoon92 <wsjtjdwns92@naver.com>
2024-10-30 18:26:19 +0900

    movie api활용 영화리스트 출력

--- javascript/1028/실습.js
+++ javascript/1028/실습.js
@@ -1,4 +1,5 @@
 // 현재 상영 중인 영화 목록을 가져오는 비동기 함수 정의
+// https://api.themoviedb.org/3/movie/now_playing?api_key=3b7e5eb620d6d28a0a820a15350f2989&language=ko
 async function getNowPlayingMovies() {
   try {
     // API의 기본 URL과 경로 설정
@@ -31,29 +32,57 @@ async function getNowPlayingMovies() {
     for (let movie of movies) {
       // 각 영화의 배경 이미지 경로를 변수에 저장
       const movieImg = movie.backdrop_path;
+      // 각 영화의 제목을 변수에 저장
+      const movieTitle = movie.title;
+      // 각 영화의 평점을 변수에 저장
+      const popularity = movie.popularity;
+      // 제일큰 평점을 계산하기위한 제일낮은값 변수에 할당
+      let maxpopularity = -Infinity;
+      if (popularity > maxpopularity) {
+        maxpopularity = popularity;
+        console.log(popularity);
 
-      // movieList 변수에 <li></li>태그 넣을준비
-      const movieList = document.createElement('li');
-      // movieLink 변수에 <a></a>태그 넣을준비
-      const movieLink = document.createElement('a');
-      // movieImage 변수에 <img></img>태그 넣을준비
-      const movieImage = document.createElement('img');
+        // movieList 변수에 <li></li>태그 넣을준비
+        const movieList = document.createElement('li');
+        // movieRank 변수에 <h3></h3>태그 넣을준비
+        const movieRank = document.createElement('h3');
+        // movieLink 변수에 <a></a>태그 넣을준비
+        const movieLink = document.createElement('a');
+        // span 변수에 <span></span>태그 넣을준비
+        const span = document.createElement('span');
+        // movieImage 변수에 <img></img>태그 넣을준비
+        const movieImage = document.createElement('img');
 
-      // movieBox자식요소로 movieList(<li></li>)넣기
-      movieBox.appendChild(movieList);
-      // movieList자식요소로 movieImage(<a></a>)넣기
-      movieList.appendChild(movieLink);
-      // movieList자식요소로 movieImage(<img></img>)넣기
-      movieLink.appendChild(movieImage);
-      movieLink.href = movieImg;
-      // 이미지 요소에 src 속성을 설정하여 해당 영화의 배경 이미지가 표시되도록 함
-      movieImage.setAttribute(
-        'src',
-        `https://image.tmdb.org/t/p/w500${movieImg}`
-      );
+        // movieBox자식요소로 movieList(<li></li>)넣기
+        movieBox.appendChild(movieList);
+        // movieList자식요소로 movieImage(h3></h3>)을 넣으면서 태그안에 API에서 뽑아온 평점출력
+        movieList.appendChild(movieRank).innerText = '평점: ' + maxpopularity;
+        // movieList자식요소로 movieImage(<a></a>)넣기
+        movieList.appendChild(movieLink);
+        // movieLink자식요소로 movieImage(<img></img>)넣기
+        movieLink.appendChild(movieImage);
+        // movieList자식요소로 movieImage(<span></span>)을 넣으면서 태그안에 API에서 뽑아온 title넣기
+        movieLink.appendChild(span).innerText = movieTitle;
 
-      // movieImage.src = `https://image.tmdb.org/t/p/w500${movieImg}`
-    }
+        // a태그의 href속성 안에 api에서 뽑아온 이미지 넣기
+        (movieLink.href = `https://image.tmdb.org/t/p/w500/${movieImg}`),
+          (target = '_blank');
+        // console.log(movieLink);
+
+        // const div = document.createElement('div')
+        // div.innerHTML = `<li><a href="${}" target="_blank"><img src="https://image.tmdb.org/t/p/w500/xlkclSE4aq7r3JsFIJRgs21zUew.jpg"><span>테리파이어 3</span></a></li>`
+
+        // https://image.tmdb.org/t/p/w500/kX9gzHyfwXCkTszBcPekQZxPnzP.jpg
+        // https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg
+        // https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+        // https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
+        // 이미지 요소에 src 속성을 설정하여 해당 영화의 배경 이미지가 표시되도록 함
+        movieImage.setAttribute(
+          'src',
+          `https://image.tmdb.org/t/p/w500${movieImg}`
+        );
+      } // if end
+    } // for end
   } catch (error) {
     console.error('Error:', error);
   }
