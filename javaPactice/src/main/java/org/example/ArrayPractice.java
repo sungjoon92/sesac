5c77f827b3dea1c610a5da63de2f78caa80c6098
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-11 14:12:00 +0900

    ë©ããí배열 연습

---
+++ javaPactice/src/main/java/org/example/ArrayPractice.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        int number;
+        number = 10;
+
+        int[] numbers1;
+        numbers1 = new int[5];
+
+        int[] numbers2;
+        numbers2 = new int[] {1, 2, 3, 4, 5};
+
+        int[] numbers3 = {1, 2, 3, 4, 5};
+        int[] numbers4 = new int[5];
+
+        String s = Arrays.toString(numbers1);
+
+        System.out.println(Arrays.toString(numbers1));
+        System.out.println(Arrays.toString(numbers2));
+        System.out.println(Arrays.toString(numbers3));
+
+        System.out.println(numbers2 == numbers3);
+        System.out.println(Arrays.equals(numbers2, numbers3));
+
+        System.out.println(numbers2.length);
+
+
+        char[] chars= {'a', 'b', 'c', 'd'};
+        for (int i = 0; i < chars.length; i++){
+            char element = chars[i];
+            System.out.println(element);
+        }
+
+        // iter
+        for (char element : chars) {
+            System.out.println(element);
+        }
+
+
+    }
+}
