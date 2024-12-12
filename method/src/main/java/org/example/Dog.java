3dcbdfb4b21ca84429d29eeb19e7968271bce944
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-12 17:47:16 +0900

    강아지 견종,이름,행동 구하기

---
+++ method/src/main/java/org/example/Dog.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Dog {
+    String dogType;
+    String name;
+    String doging;
+
+    public Dog(String dogType, String name, String doging) {
+        this.dogType = dogType;
+        this.name = name;
+        this.doging = doging;
+    }
+
+    void seat(){
+        System.out.println("나 "+this.name+ "앉았다");
+    }
+
+    void hand(){
+        System.out.println(this.name + "손");
+    }
+
+    String dogs(){
+
+        return "견종: " + dogType +" 이름: "+ name +" 행동: "+ doging;
+    }
+
+}
