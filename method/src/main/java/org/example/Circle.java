5d546a4e38821c53cd3c322a39a580bc925d6e33
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-12 17:46:52 +0900

    원의 둘레 구하기

---
+++ method/src/main/java/org/example/Circle.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Circle {
+        int radius;
+
+        Circle(int radius){
+            this.radius = radius;
+        }
+
+        double calclaleArea(){
+            return 3.14 * radius * radius;
+        }
+}
