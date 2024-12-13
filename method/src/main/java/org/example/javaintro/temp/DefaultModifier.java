f9c992307ac3caa1d1034151cf40ac490ad4e52c
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-13 18:06:49 +0900

    게터 세터 클래스 만들기

---
+++ method/src/main/java/org/example/javaintro/temp/DefaultModifier.java
@@ -0,0 +1,22 @@
+package org.example.javaintro.temp;
+
+public class DefaultModifier {
+    public String publicString = "public";
+    String privateString = "default";
+
+    public String getPublicString() {
+        return publicString;
+    }
+
+    public void setPublicString(String publicString) {
+        this.publicString = publicString;
+    }
+
+    public String getPrivateString() {
+        return privateString;
+    }
+
+    public void setPrivateString(String privateString) {
+        this.privateString = privateString;
+    }
+}
