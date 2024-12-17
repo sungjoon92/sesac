2f6c2b82c7dea65fdf1dcae411ebd3d3d433ccf3
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-17 17:36:15 +0900

    강아지클래스 Animal 상속

---
+++ method/src/main/java/org/example/interfaceprac/Dog.java
@@ -0,0 +1,17 @@
+package org.example.interfaceprac;
+
+public class Dog extends Animal {
+
+    private String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed=breed;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("왕왕");
+    }
+
+}
