94a09dd1e33e8008f4ba1e6563f493318a4860bc
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-17 17:39:41 +0900

    사람인원탑승 인터페이스

---
+++ method/src/main/java/org/example/interfaceprac/vegicle/PassengerBoardable.java
@@ -0,0 +1,12 @@
+package org.example.interfaceprac.vegicle;
+
+public interface PassengerBoardable {
+
+    void boardMethod();
+
+//    void boardPassenger();
+//
+//    void getOffPassenger();
+//
+//    void getPassengerCount();
+}
