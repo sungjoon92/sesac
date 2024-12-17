652e33e8cfd4fba4e5208a27e39cff6cbd3d4342
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-17 17:39:17 +0900

    몬스터 정보

---
+++ method/src/main/java/org/example/Game/Monster.java
@@ -0,0 +1,13 @@
+package org.example.Game;
+
+public class Monster {
+    public String name;
+    public int level;
+    public int damage;
+
+    public Monster(String name, int level, int damage) {
+        this.name = name;
+        this.level = level;
+        this.damage = damage;
+    }
+}
