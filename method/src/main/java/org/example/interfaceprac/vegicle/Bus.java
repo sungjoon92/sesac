f0d5871d84a7d6101a6c4d0a06db75db0dbee18c
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-17 17:31:07 +0900

    버스 클래스 추상화,인터페이스 연습

---
+++ method/src/main/java/org/example/interfaceprac/vegicle/Bus.java
@@ -0,0 +1,19 @@
+package org.example.interfaceprac.vegicle;
+
+public class Bus extends Vehicle implements PassengerBoardable{
+
+    @Override
+    public void boardMethod() {
+        System.out.println("버스: 보드 메소드");
+    }
+
+    @Override
+    void fuelMethod() {
+        System.out.println("버스: 연료 메소드");
+    }
+
+    @Override
+    void speedMethod() {
+        System.out.println("버스: 차량 스피드 메소드");
+    }
+}
