aae28b1ac917ecae22202b2b845e3061854560c7
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-17 17:37:58 +0900

    버스, 트럭, 버트럭스 메인(상속, 인터페이스 연습)

---
+++ method/src/main/java/org/example/interfaceprac/vegicle/Main.java
@@ -0,0 +1,25 @@
+package org.example.interfaceprac.vegicle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+
+        bus.boardMethod();
+        bus.fuelMethod();
+        bus.speedMethod();
+
+
+        Truck truck = new Truck();
+        truck.lendMethod();
+        truck.fuelMethod();
+        truck.speedMethod();
+
+
+        Brucks brucks = new Brucks();
+        brucks.speedMethod();
+        brucks.boardMethod();
+        brucks.fuelMethod();
+        brucks.lendMethod();
+    }
+}
