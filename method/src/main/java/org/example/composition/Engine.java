2f7dbe8453eaf906af781846b6949a3cd3e811eb
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-18 17:52:36 +0900

    Engine클래스 (현재마력)

---
+++ method/src/main/java/org/example/composition/Engine.java
@@ -0,0 +1,9 @@
+package org.example.composition;
+
+public class Engine {
+    public int horsepower;
+
+    public Engine(int horsepower) {
+        this.horsepower = horsepower;
+    }// Engine() end
+}
