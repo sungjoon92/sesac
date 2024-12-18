184560bca95d694fb52b9cf34b949471c0795361
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-18 13:12:20 +0900

    워리어 클래스 (전직 추가)

--- method/src/main/java/org/example/Game/Warrior.java
+++ method/src/main/java/org/example/Game/Warrior.java
@@ -5,7 +5,7 @@ public class Warrior extends Character {
     public int angerGauge;
 
     public Warrior(String name, int level, String characterClass) {
-        super(name,level, characterClass);
+        super(name, level, characterClass);
         this.angerGauge = 0;
     }
 
@@ -15,17 +15,29 @@ public class Warrior extends Character {
         if (this.characterClass.equals("모험가") && this.level >= 10) {
             this.characterClass = "워리어";
             this.level = 1;
+            this.hp = 120;
+            this.damage = 10;
             return "전직완료: " + this.characterClass;
         } else {
             return "전직 조건을 만족하지 못했습니다.";
         } //if end
     }// WarriorClassChange() end
 
+    // 워리어 레벨업
     public String levelUp() {
         if (this.characterClass.equals("워리어")) {
-            levelUp();
+            this.level++;
+            this.hp += 30;
+            this.angerGauge += 10;
+            this.damage += 10;
+
+            return "레벨 업! 현재레벨: " + level + " 증가된 데미지: 10 현재 데미지: " + damage + " 체력: " + hp;
+        }else {
+            return null;
+
         }
-        return null;
-    }
+
+    }// levelUp() end
+
 
 }
