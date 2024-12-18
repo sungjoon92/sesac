3d8db2d42168a0968c462b851c2011978fa13482
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-18 13:11:29 +0900

    트럭 클래스 클래스(상속, 인터페이스 연습)

---
+++ method/src/main/java/org/example/interfaceprac/vegicle/Truck.java
@@ -0,0 +1,14 @@
+package org.example.interfaceprac.vegicle;
+
+public class Truck extends Vehicle implements CargoLendable{
+    @Override
+    public void lendMethod() {
+        System.out.println("트럭: 랜드 메소드");
+    }
+
+    @Override
+    void fuelMethod() {
+        System.out.println("트럭: 연료 메소드");
+
+    }
+}
