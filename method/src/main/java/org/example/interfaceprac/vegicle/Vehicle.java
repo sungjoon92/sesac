973c39fb545c0b097f808ed5fac70a8c0b18c0e5
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-18 13:11:44 +0900

    차량 클래스(상속, 인터페이스 연습)

---
+++ method/src/main/java/org/example/interfaceprac/vegicle/Vehicle.java
@@ -0,0 +1,16 @@
+package org.example.interfaceprac.vegicle;
+
+public abstract class Vehicle {
+
+    protected int maxfuel = 100;
+
+    // 연료 관련
+    abstract void fuelMethod();
+    // 속도 관련
+//    abstract void speedMethod();
+
+    void speedMethod() {
+        System.out.println("차량 스피드 메소드");
+    }
+    
+}
