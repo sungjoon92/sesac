c358de92878871540435d18eec27b9865b729eba
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-19 18:11:16 +0900

    공격상태(composition연습)

---
+++ method/src/main/java/org/example/composition/SpecialAttackable.java
@@ -0,0 +1,5 @@
+package org.example.composition;
+
+public interface SpecialAttackable {
+    void specialAttack();
+}
---
+++ method/src/main/java/org/example/composition/Tool.java
@@ -0,0 +1,5 @@
+package org.example.composition;
+
+public interface Tool {
+    void use();
+}
