779c756b163cd9b22677983c94f0d5dacb9d3210
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-20 11:00:56 +0900

    학생모음 매니저 클래스 (Map, List)

---
+++ method/src/main/java/org/example/collectionprac/StudentManager.java
@@ -0,0 +1,42 @@
+package org.example.collectionprac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManager {
+// 학생들의 목록을 관리 (가지고 있음)
+    // 학생 추가 검색
+    // 학생들의 정보 조회
+
+    private final List<Student> students;
+
+    // 기본 생성자
+    public StudentManager() {
+        this.students = new ArrayList<>();
+    }// StudentManager() end
+
+    // 학생정보 저장
+    public List<Student> addStudent(Student student) {
+        students.add(student);
+        return students;
+    }// addStudent() end
+
+    // 학생들의 정보조회
+    public void showStudentInfo() {
+        for (Student student : students) {
+            student.showInfo();
+            System.out.println();
+        }// for end
+    }// showStudentInfo() end
+
+    // 학생 검색
+    public Student findStudentByName(String name) {
+        // 이름이 같은 학생을 return
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
+                return student;
+            }// if end
+        }// for end
+        return null;
+    }// findStudentByName() end
+}// class end
