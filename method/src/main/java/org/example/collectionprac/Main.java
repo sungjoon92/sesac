5b25c70f484566ee9a982f709202377469a6fb0b
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-20 11:00:06 +0900

    학생 정보 추가, 조회 메인

--- method/src/main/java/org/example/collectionprac/Main.java
+++ method/src/main/java/org/example/collectionprac/Main.java
@@ -2,48 +2,55 @@ package org.example.collectionprac;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // 학생
+        // 학생 정보 저장
         Student jooni = new Student("쮸니", 3);
         Student tengi = new Student("탱이", 5);
         Student gooi = new Student("구이", 7);
 
-        HashMap<String, Integer> studentMap = new HashMap<>();
-        studentMap.put(jooni.name,jooni.age);
-        studentMap.put(tengi.name,tengi.age);
-        studentMap.put(gooi.name,gooi.age);
-
-        System.out.println(studentMap);
-
-        // 과목
-        ArrayList<String> subjects = new ArrayList<>();
+        // 과목 저장
+        List<String> subjects = new ArrayList<>();
         subjects.add("국어");
         subjects.add("영어");
         subjects.add("수학");
         subjects.add("과학");
         subjects.add("사회");
 
-        System.out.println(subjects);
 
+        // 점수 넣기
+        jooni.addGrade(subjects.get(0), 10);
+        jooni.addGrade(subjects.get(1), 30);
+        jooni.addGrade(subjects.get(2), 20);
+        jooni.addGrade(subjects.get(3), 60);
+        jooni.addGrade(subjects.get(4), 50);
 
-        jooni.subjectScore("국어", 50);
+        // 대상학생의 정보 조회
+        jooni.showInfo();
 
-        System.out.println(jooni);
+        tengi.addGrade("국어", 10);
+        tengi.addGrade("영어", 30);
+        tengi.addGrade("수학", 20);
+        tengi.addGrade("과학", 60);
 
+        // 대상 학생의 평균 점수 조회
+        System.out.println(jooni.averageScore());
 
-        // 점수
+        // studentManager에 학생 정보 저장
+        StudentManager studentManager = new StudentManager();
+        studentManager.addStudent(jooni);
+        studentManager.addStudent(tengi);
+        studentManager.addStudent(gooi);
+        
+        // 저장된 학생 정보 조회
+        studentManager.showStudentInfo();
 
-//        학생1이 있다.
-//        학생2가 있다.
-//
-//        학생 관리 시스템이 있다.
-//
-//        학생 관리 시스템이 학생1을 관리하도록 핮.
-//        학생관리시스템.니가관리하는학생들의목록을보여줘()
-
-    }
-}
+        //검색
+        Student b = studentManager.findStudentByName("구이");
+        b.showInfo();
+    }// main end
+}// class end
