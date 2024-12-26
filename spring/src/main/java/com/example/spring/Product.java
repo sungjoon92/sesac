6d9e0d54a32f40b5c5f2aa07bded527692665285
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-26 22:12:59 +0900

    상품 정보 클래스

--- spring/src/main/java/com/example/spring/Product.java
+++ spring/src/main/java/com/example/spring/Product.java
@@ -1,5 +1,15 @@
 package com.example.spring;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+    private Long id;
+    private String name;
+    private int price;
 
 }
