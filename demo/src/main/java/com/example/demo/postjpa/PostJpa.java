c26c939f7e88225af002b158d36187cfab2276a4
sungjoon92 <wsjtjdwns92@naver.com>
2024-12-30 17:20:25 +0900

    jap postEntity

---
+++ demo/src/main/java/com/example/demo/postjpa/PostJpa.java
@@ -0,0 +1,32 @@
+package com.example.demo.postjpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "postJpa")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostJpa {
+    @Id
+    // sql의 autoincrement 기능
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+
+    public PostJpa update(String title, String content) {
+        this.title = title;
+        this.content = content;
+        return this;
+    }
+
+}
